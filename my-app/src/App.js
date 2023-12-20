5733686f4efd69b48be0e43525050e9ea7ef0331
Timofey <stoganych@gmail.com>
2023-12-20 20:50:16 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+  return (
+    <div id="quote-box">
+      <div id="text">
+        <i class="fa fa=quote-left"></i>
+        <span>{text}</span>
+      </div>
+      <div id="author">{author}</div>
+      <button id="new-quote" onChange={}></button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet"></a>
+    </div>
+  );
+}
+
+export default App;
